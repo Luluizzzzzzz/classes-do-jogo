f046dc0c18445c546ee6ba7909bb524494e36525
Luiz <luiz-.gustavo@outlook.com>
2023-12-28 12:15:27 -0300

    enviando

---
+++ index.js
@@ -0,0 +1,25 @@
+    let heroi = {
+        nome: "Luiz",
+        idade: 30,
+        class: "Ninja"
+    };
+   
+   switch(heroi.class) {
+
+    case heroi.class = "Guerreiro":
+        heroi.arma = "Espada"
+        break;
+
+    case heroi.class = "Mago":
+        heroi.arma = "Magia"
+        break;
+
+    case heroi.class = "Monge":
+        heroi.arma = "Artes Marciais"
+        break;
+
+    case heroi.class = "Ninja":
+        heroi.arma = "Shuriken"
+        break;
+   }
+   console.log(" O " + heroi.class + " atacou usando " +  heroi.arma );
